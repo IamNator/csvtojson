0316a9277a76646e4d36b5f880ee7f3dfce2171f
Nator Verinumbe <natverior1@gmail.com>
2020-04-19 09:03:38 +0100

    Assignment done

---
+++ toJson.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const converter = require('csvtojson')
+const path = require('path')
+// const csvFilePath = './data/customer-data.csv'
+// const jsonFilePath = './data/customer-data.json'
+
+
+const convert = ( csvFilePath = './data/customer-data.csv', jsonFilePath = './data/customer-data.json' ) =>
+{
+
+converter()
+    .fromFile(csvFilePath)
+    .then((jsonObject) => {
+      //console.log(jsonObject)
+      fs.writeFileSync(jsonFilePath, JSON.stringify(jsonObject) )
+      console.log('Converted to json and saved in ', jsonFilePath)
+    })
+    //
+    // fs.writeFile(jsonFilePath, jsonObject, 'utf8', (error) => {
+    //   if (error) {console.log(error.message)}
+    //   console.log('Converted to json and saved in ', jsonFilePath)
+    // } )
+
+}
+
+
+//node toJson.js './data/customer-data.csv'  './data/customer-data.json'
+convert(process.argv[2], process.argv[3])
